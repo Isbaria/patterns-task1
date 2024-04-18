f3de1e2f7def52638d5a1824dd8a80e725cacd95
Alexandr <isbarius@yandex.ru>
2024-04-18 15:28:51 +0300

    Initial commit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,55 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private static final Faker faker = new Faker(new Locale("ru"));
+    private static Random random = new Random();
+    private static String[] cities = {
+            "Москва",
+            "Санкт-Петербург",
+            "Казань",
+            "Екатеринбург",
+            "Тюмень",
+            "Иркутск",
+            "Новосибирск",
+            "Омск",
+            "Владивосток",
+            "Нижний Новгород"
+    };
+
+    private DataGenerator() {
+    }
+
+    public static String generateCity() {
+        int index = random.nextInt(cities.length);
+        return cities[index];
+    }
+
+    public static String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public static String generateFullName() {
+
+        return faker.name().fullName();
+    }
+
+    public static String generatePhoneNumber() {
+
+        return faker.numerify("+7 (###) ### ## ##");
+    }
+
+    @Value //рекомендуется аннотировать Value
+    public static class UserInfo { //дата класс - специальный класс, который служит для задания структуры объекта с данными, т.е. описываются те поля, которые должны быть в объекте
+        private String city;
+        private String name;
+        private String phone;
+    }
+}
---
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -0,0 +1,43 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardDeliveryTest {
+    @BeforeEach
+    void SetUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTest() {
+        String city = DataGenerator.generateCity();
+        String firstDate = DataGenerator.generateDate(5, "dd.MM.yyyy");
+        String secondDate = DataGenerator.generateDate(7, "dd.MM.yyyy");
+        String fullName = DataGenerator.generateFullName();
+        String phoneNumber = DataGenerator.generatePhoneNumber();
+
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys(firstDate);
+        $("[data-test-id=name] input").setValue(fullName);
+        $("[data-test-id=phone] input").setValue(phoneNumber);
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(Condition.hidden).shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstDate), Duration.ofSeconds(15));
+        $(".button").click();
+        $("[data-test-id=date] input").doubleClick().sendKeys(secondDate);
+        $(".button").click();
+        $("[data-test-id='replan-notification'] .notification__title").shouldHave(Condition.exactText("Необходимо подтверждение"));
+        $(".notification__content .button").click();
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondDate));
+    }
+}
